c00964c80d73bc1c628f3d8896baedc458841cbe
RayzalS <149979448+RayzalS@users.noreply.github.com>
2024-02-22 14:06:32 +0700

    Test

--- app/src/androidTest/java/com/example/anmp_week1/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/anmp_week1/ExampleInstrumentedTest.kt
@@ -20,5 +20,6 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.anmp_week1", appContext.packageName)
+        //fjiejfoheeofwiwoiefhewihfpi
     }
 }
